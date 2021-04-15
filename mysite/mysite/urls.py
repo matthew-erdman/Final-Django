c365cfbb319034c9e8098189480e7ce3b8585fc0
matthew-erdman <matthew.erdman.23@bishops.com>
2021-04-14 23:02:21 -0700

    Imported user model

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -17,6 +17,6 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path('wiki/', include('wiki.urls')),
+    path('', include('wiki.urls')),
     path('admin/', admin.site.urls),
 ]
